fef61f5d774510e5b2cd497ffb04a2a74751ac3f
Aman Kumar <amankumar34276@gmail.com>
2022-01-23 22:33:21 +0530

    Successfully created buy and sell app

--- src/components/App.js
+++ src/components/App.js
@@ -13,18 +13,6 @@ class App extends Component {
     await this.loadBlockchainData()
   }
 
-  // async loadWeb3() {
-  //   if (window.ethereum) {
-  //     window.web3 = new Web3(window.ethereum)
-  //     await window.ethereum.enable()
-  //   }
-  //   else if (window.web3) {
-  //     window.web3 = new Web3(window.web3.currentProvider)
-  //   }
-  //   else {
-  //     window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-  //   }
-  // }
 
   async loadWeb3() {
     if (window.ethereum) {
@@ -34,15 +22,13 @@ class App extends Component {
     }else{
       window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
-    //return false;
+    
   }
 
   async loadBlockchainData() {
     const web3 = window.web3
     // Load account
     const accounts = await web3.eth.getAccounts()
-    //const accounts = await window.ethereum.request({ method: 'eth_accounts' });
-    //const accounts = await window.ethereum.send('eth_requestAccounts');
     this.setState({ account: accounts[0] })
     const networkId = await web3.eth.net.getId()
     const networkData = Marketplace.networks[networkId]
@@ -50,8 +36,16 @@ class App extends Component {
       const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
       this.setState({ marketplace })
       const productCount = await marketplace.methods.productCount().call()
-      console.log(productCount.toString())
+      this.setState({ productCount })
+      // Load products
+      for (var i = 1; i <= productCount; i++) {
+        const product = await marketplace.methods.products(i).call()
+        this.setState({
+          products: [...this.state.products, product]
+        })
+      }
       this.setState({ loading: false})
+      console.log(this.state.products)
     } else {
       window.alert('Marketplace contract not deployed to detected network.')
     }
@@ -67,11 +61,20 @@ class App extends Component {
     }
 
     this.createProduct = this.createProduct.bind(this)
+    this.purchaseProduct = this.purchaseProduct.bind(this)
   }
 
   createProduct(name, price) {
     this.setState({ loading: true })
     this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  purchaseProduct(id, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
     .once('receipt', (receipt) => {
       this.setState({ loading: false })
     })
@@ -85,7 +88,10 @@ class App extends Component {
           <div className="row">
           <main role="main" className="col-lg-12 d-flex">
           { 
-          <Main createProduct={this.createProduct} />
+          <Main 
+            products={this.state.products}
+            createProduct={this.createProduct}
+            purchaseProduct={this.purchaseProduct} />
           // this.state.loading
           //   ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
           //   : <Main createProduct={this.createProduct} />
